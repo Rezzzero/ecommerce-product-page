bfb7fe6fcb38997669114a2cf0c2293c5dca1535
Pasha Rezero <kivachuk.2018@gmail.com>
2025-04-17 14:30:57 +0300

    hot fix

--- src/components/product/ProductModal.tsx
+++ src/components/product/ProductModal.tsx
@@ -20,7 +20,7 @@ export const ProductModal = ({ closeModal }: { closeModal: () => void }) => {
         onClick={closeModal}
       />
 
-      <div className="fixed z-20 left-1/3 top-1/4 w-[460px]">
+      <div className="fixed z-20 left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 w-[460px]">
         <img
           src={closeModalIcon}
           alt="close modal"
